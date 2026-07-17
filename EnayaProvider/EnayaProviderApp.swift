e9857b33a725d70bd1ee2ca31a1f484f67fee5d3
monazarea <monazarea3@gmail.com>
2026-07-16 21:42:53 -0400

    first commit

---
+++ EnayaProvider/EnayaProviderApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnayaProviderApp.swift
+//  EnayaProvider
+//
+//  Created by Mona Zarea on 16/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct EnayaProviderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
