5d86f4dd49269e554555ac37c1f69bf7bc54e1f6
MahmoudRaafat <mahmoudrafatt995@gmail.com>
2026-08-07 14:14:41 -0700

    feat(home-ws-connection): implement STOMP websocket client and network configurations

--- EnayaProvider/Core/Network/NetworkConfiguration.swift
+++ EnayaProvider/Core/Network/NetworkConfiguration.swift
@@ -11,4 +11,5 @@ import Foundation
 
 enum NetworkConfiguration {
     static let baseURL = "https://home-nursing-system-production.up.railway.app"
+    static let socketURL = "ws://home-nursing-system-production.up.railway.app/ws"
 }
